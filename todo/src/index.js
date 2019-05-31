389eff186a7ab000f6faba1baf5b68739ff1ce8d
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-05-31 15:13:03 +0300

    Create react components

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,19 +2,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// create react element, use JSX
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input type="text" placeholder="search"/>
+// create react component
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
-  </div>
-);
-//const el = React.createElement('h1', null, 'Hello, World');
-console.log(el);
+  );
+};
 
-  // use REACTDOM for render this element
-ReactDOM.render(el, document.getElementById('root'));
+// create another component
+const AppHeader = () => {
+  return (
+    <h1>My Todo List</h1>
+  );
+};
+
+// create another component
+const SearchPanel = () => {
+  return (
+    <input type="text" placeholder="search"/>
+  );
+};
+
+// create another component
+const App = () => {
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  );
+};
+
+
+//const el = React.createElement('h1', null, 'Hello, World');
+
+
+  // using REACTDOM for render this element
+ReactDOM.render(<App/>, document.getElementById('root'));
