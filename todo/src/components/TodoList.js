c543dced37ee60902a9d1d2ae46eb58ec44fdc2e
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-06-03 15:09:50 +0300

    function map and spread operator

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import TodoListItem from './TodoListItem';
-const TodoList = () => {
+const TodoList = ( {todos} ) => {
+
+  const elements = todos.map((item)=> {
+    return (
+      <li><TodoListItem {...item} />
+      </li>
+    );
+  });
+
   return (
     <ul>
-      <li><TodoListItem label="Drink Coffee" /></li>
-      <li><TodoListItem
-        label="Build React App"
-        important /></li>
+      { elements }
     </ul>
   );
 };
--- todo/src/index.js
+++ todo/src/index.js
@@ -7,11 +7,28 @@ import SearchPanel from './components/SearchPanel';
 
 // create another component
 const App = () => {
+
+  const todoData = [
+    {
+      label: 'Drink Coffee',
+      important: false
+    },
+    {
+      label: 'Make Awesome App',
+      important: true
+    },
+    {
+      label: 'Have a lunch',
+      important: false
+    },
+
+  ];
+
   return (
     <div>
       <AppHeader/>
       <SearchPanel/>
-      <TodoList/>
+      <TodoList todos={todoData} />
     </div>
   );
 };
