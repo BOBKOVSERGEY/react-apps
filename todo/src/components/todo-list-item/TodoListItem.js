f70fa15eee683fa73510a82216e97f14f82d12da
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-06-04 16:28:30 +0300

    Обработчики событий и управление состоянием

--- todo/src/components/todo-list-item/TodoListItem.js
+++ todo/src/components/todo-list-item/TodoListItem.js
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ( { label, important = false } ) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
+export default class TodoListItem extends Component {
+
+  /*constructor() {
+    super();
+
+    this.onLabelClick = () => {
+      console.log(`Done ${this.props.label}`)
+    }
+  }*/
+
+  constructor() {
+    super();
+
+    this.state = {
+      done: false,
+      important: false
+    }
+  }
+
+  onLabelClick = () => {
+    this.setState( ({done}) => {
+      return {
+        done: !done
+      }
+    });
   };
 
-  return (
-    <span className="todo-list-item" style={style}>
+  onMarkImportant = () => {
+    this.setState((state) => {
+      return {
+        important: !state.important
+      }
+    });
+  };
+
+
+  render() {
+    const { label } = this.props;
+
+    const { done, important } = this.state;
+
+
+    let classNames = 'todo-list-item';
+
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
+
+    return (
+      <span className={ classNames }>
       <span
         className="todo-list-item-label"
-        style={style}>
+        onClick={this.onLabelClick}>
         {label}
       </span>
       <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={this.onMarkImportant}>
         <i className="fa fa-exclamation" />
       </button>
 
@@ -25,7 +72,6 @@ const TodoListItem = ( { label, important = false } ) => {
       </button>
     </span>
 
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
